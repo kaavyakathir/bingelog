e4b03d064bb092bc0f6ad22e42a21fde43e9eddc
kaavyakathir <kaavyakathir@gmail.com>
2026-09-06 21:05:33 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { supabase } from './supabaseClient'
+console.log(supabase)
 import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
